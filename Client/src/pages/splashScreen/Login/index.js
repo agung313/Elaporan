f616f929feebf5a453a90dd8094d2f41d38e6bf9
byondri <ondrinurdiansyah@gmail.com>
2023-07-18 08:43:21 +0700

    fetch API

--- Client/src/pages/splashScreen/Login/index.js
+++ Client/src/pages/splashScreen/Login/index.js
@@ -3,25 +3,26 @@ import React, { useState } from 'react'
 import { BgMain, EmailIcon, LgBappeda, PasswordIcon } from '../../../assets/images'
 import axios from 'axios';
 
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { err } from 'react-native-svg/lib/typescript/xml';
+
 const LoginSide = ({navigation}) => {
+
+    const base_url = 'http://10.0.2.2:8000/api';
 
     const [inputs, setInputs] = useState({
         username:'',
         password:''
     })
 
-    const [validationStatus, setValidationStatus] = useState({
-        username:true,
-        password: true
+
+    const handleChangeInput  = (inputName, text)=>{
+        setInputs({
+            ...inputs,
+            [inputName]:text
+        })
     }
     
-    // const validateInputs = () =>{
-    //     const isValid ={
-    //         name: inputs.name
-    //     }
-    // }
-
-
 
     const WindowWidth = Dimensions.get('window').width;
     const WindowHeight = Dimensions.get('window').height;
@@ -29,12 +30,31 @@ const LoginSide = ({navigation}) => {
     const handlerLogin = async data =>{
 
         try {
-          const response =  await axios.get('http://10.0.2.2:8000/api/auth/tes');
-            console.log(response.data)
-           } catch (error) {
-            console.log(error)
+            let dataLogin ={
+                email: inputs.username,
+                password: inputs.password
+            }
 
-          }
+            let response = await axios.post('http://10.0.2.2:8000/api/auth/login', dataLogin)
+
+            console.log(response.data.data.token,"<--- response login");
+
+            await AsyncStorage.setItem('AccessToken', response.data.data.token)
+            let myToken = await AsyncStorage.getItem('AccessToken')
+            console.log(myToken, "<---- my token")
+
+            // if (res.data.data.role === 'kasum') {
+
+            //     navigation.replace('SplashLogin'); 
+
+            // }else if (res.data.data.role === 'thl') {
+            //     console.log('as', 'thl ---')
+
+                navigation.replace('AppScreen');
+            // }
+        } catch (error) {
+            console.log(error, "<--- error")
+        }
 
     }
     return (
@@ -51,9 +71,9 @@ const LoginSide = ({navigation}) => {
                     <TextInput
                         placeholder='Username'
                         placeholderTextColor={"#000"}
-                        value={username}
+                        value={inputs.username}
                         keyboardType= "default"
-                        onChangeText={(text) => setUserName(text)}
+                        onChangeText={(text) => handleChangeInput('username',text)}
                         style={{ color: "#000" }}
                     />
                 </View>
@@ -69,9 +89,9 @@ const LoginSide = ({navigation}) => {
                     <TextInput
                         placeholder='Password'
                         placeholderTextColor={"#000"}
-                        value={password}
+                        value={inputs.password}
                         keyboardType= "default"
-                        onChangeText={(text) => setPassword(text)}
+                        onChangeText={(text) => handleChangeInput('password',text)}
                         style={{ color: "#000" }}
                         secureTextEntry={true}
                         textContentType='password'
@@ -84,12 +104,12 @@ const LoginSide = ({navigation}) => {
                     <Text style={{color:"#000", fontSize:12, fontWeight:"bold"}}>Lupa Password ?</Text>
                 </TouchableOpacity>
             </View>
-            <TouchableOpacity style={{width:200, height:40, backgroundColor:"green", borderRadius:15, elevation:10, alignItems:"center", justifyContent:"center", marginTop:30}} onPress={() => navigation.navigate("AppScreen")}>
+            {/* <TouchableOpacity style={{width:200, height:40, backgroundColor:"green", borderRadius:15, elevation:10, alignItems:"center", justifyContent:"center", marginTop:30}} onPress={() => navigation.navigate("AppScreen")}>
                 <Text style={{color:"#fff", fontWeight:"bold", fontSize:18}}>THL</Text>
             </TouchableOpacity>
             <TouchableOpacity style={{width:200, height:40, backgroundColor:"green", borderRadius:15, elevation:10, alignItems:"center", justifyContent:"center", marginTop:30}} onPress={() => navigation.navigate("KasumScreen")}>
                 <Text style={{color:"#fff", fontWeight:"bold", fontSize:18}}>KASUM</Text>
-            </TouchableOpacity>
+            </TouchableOpacity> */}
             <TouchableOpacity style={{width:200, height:40, backgroundColor:"green", borderRadius:15, elevation:10, alignItems:"center", justifyContent:"center", marginTop:30}} onPress={handlerLogin}>
                 <Text style={{color:"#fff", fontWeight:"bold", fontSize:18}}>Login</Text>
             </TouchableOpacity>            
